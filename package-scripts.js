af575f8f8267fe45ff160b5c6b5b0be6ac639632
Aaron Tribou <aaron.tribou@gmail.com>
2016-07-02 18:08:02 -0500

    Switch code coverage to babel-istanbul

--- package-scripts.js
+++ package-scripts.js
@@ -23,7 +23,7 @@ exports.scripts = {
     default: `cross-env NODE_ENV=test babel-node ${pathTo('test')}`,
     dev: 'npm start -- test | tap-nyan',
     cov: 'cross-env NODE_ENV=test' +
-    '     babel-node node_modules/isparta/bin/isparta cover' +
+    '     babel-node node_modules/.bin/babel-istanbul cover' +
     `     --report text --report html --report lcov --dir reports/coverage ${pathTo('test')}`,
     e2e: 'cross-env NODE_ENV=development nightwatch-autorun'
   },
@@ -40,7 +40,7 @@ exports.scripts = {
       'NODE_ENV=test babel-node test > ${CIRCLE_ARTIFACTS}/test.log',
       'cat ${CIRCLE_ARTIFACTS}/test.log | tap-xunit > ${CIRCLE_TEST_REPORTS}/test.xml'
     ].join(' && '),
-    cov: 'NODE_ENV=test babel-node node_modules/isparta/bin/isparta cover ' +
+    cov: 'NODE_ENV=test babel-node node_modules/.bin/babel-istanbul cover ' +
     '     --report text --report lcov --verbose --dir ${CIRCLE_ARTIFACTS}/coverage test/index.js',
     e2e: 'REPORT_DIR=${CIRCLE_TEST_REPORTS} LOG_DIR=${CIRCLE_ARTIFACTS}' +
     '     NODE_ENV=development nightwatch-autorun',
