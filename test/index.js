bad779ebd42a98539abb807f3d3597f1edbe49bb
Nik Butenko <nik@butenko.me>
2016-07-07 17:28:05 +1000

    Use css-modules-require-hook

--- test/index.js
+++ test/index.js
@@ -1,14 +1,6 @@
 import glob from 'glob';
 
-const noop = () => null;
-
-// Skip all extra extensions
-require.extensions['.jpg'] = noop;
-require.extensions['.jpeg'] = noop;
-require.extensions['.png'] = noop;
-require.extensions['.gif'] = noop;
-require.extensions['.svg'] = noop;
-require.extensions['.css'] = noop;
-require.extensions['.ico'] = noop;
+import cssHook from 'css-modules-require-hook';
+cssHook({generateScopedName: '[name]__[local]'});
 
 glob.sync('**/*-test.js', {realpath: true, cwd: __dirname}).forEach(require);
